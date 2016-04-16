967b1a1d970f6a19fcdb558f1d56ad9ceeb669ab
vidhan13j07 <vidhanj1307@gmail.com>
2016-04-16 19:40:08 +0530

    Added User table and welcome page

--- src/Database.java
+++ src/Database.java
@@ -12,6 +12,11 @@ public class Database
 		String sql = "create table Admin_table" + "(" + "User_ID varchar(20) not null," + "Password varchar(15) not null," + "primary key(User_ID)" + ");";
 		st.executeUpdate(sql);		
 	}
+	private static void create_user(Statement st)throws SQLException
+	{
+		String sql = "Create table User_Table" + "(" + "EmailID varchar(30) not null," + "Password varchar(15) not null," + "FullName varchar(30) not null," + "Gender varchar(8) not null," + "Age int not null," + "Mobile varchar(11) not null," + "City varchar(20) not null," + "State varchar(25) not null," + "Security_question varchar(40) not null," + "Security_answer varchar(20) not null" + ")";
+		st.executeUpdate(sql);
+	}
 	public static void main(String[] args)
 	{
 		Statement stmt = null;
@@ -22,6 +27,7 @@ public class Database
 			stmt = (Statement)sq.con.createStatement();
 			create_Database(stmt);
 			create_admin(stmt);
+			create_user(stmt);
 			System.out.println("done!");
 		}
 		catch(SQLException se)
---
+++ src/Welcome.java
@@ -0,0 +1,57 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class Welcome implements ActionListener
+{
+	JFrame f;
+	JLabel l1,l2,l3;
+	Login l;
+	Signup s;
+	ImageIcon img;
+	JButton b1,b2;
+	Welcome()
+	{
+		img=new ImageIcon("indian-rail.jpg");
+		f=new JFrame("Welcome");
+		b1=new JButton("LOGIN");
+		b1.setForeground(Color.blue);
+		b1.addActionListener(this);
+		b1.setBounds(500,500,100,30);
+		b2=new JButton("SIGNUP");
+		b2.setForeground(Color.blue);
+		b2.addActionListener(this);
+		b2.setBounds(700,500,100,30);
+		f.getContentPane().add(b1);
+		f.getContentPane().add(b2);
+		f.getContentPane().setLayout(null);
+		f.getContentPane().setBackground(Color.yellow);
+		l1=new JLabel("OnLine Railway Reservation");
+		l1.setBounds(150,300,1000,50);
+		l1.setFont(new Font("Courier new",Font.BOLD,60));
+		l1.setForeground(Color.red);
+		l2=new JLabel("And Enquiry");
+		l2.setFont(new Font("Courier new",Font.BOLD,60));
+		l2.setBounds(400,380,800,60);
+		l2.setForeground(Color.red);
+		l3=new JLabel(img);
+		l3.setBounds(0,0,1300,1000);
+		f.getContentPane().add(l1);
+		f.getContentPane().add(l2);
+		f.getContentPane().add(l3);	
+		f.setSize(1300,1000);
+		f.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==b1)
+		{
+			f.setVisible(false);
+			l=new Login();
+		}
+		if(e.getSource()==b2)
+		{
+			f.setVisible(false);
+			s=new Signup();
+		}
+	}
+}
