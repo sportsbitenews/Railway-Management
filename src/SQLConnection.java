b2f2953d51fa57e452ba14fb6af2a54e31f0b89c
vidhan13j07 <vidhanj1307@gmail.com>
2016-04-20 19:36:42 +0530

    small changes

--- src/SQLConnection.java
+++ src/SQLConnection.java
@@ -3,15 +3,10 @@ public class SQLConnection
 {
     private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost/?";
-    private static String USER = "root";
-    private static String PASS = "vidhan";
+    private static final String USER = "root";
+    private static final String PASS = "vidhan";
     public Connection con = null;
     public SQLConnection() {}
-    /*public SQLConnection(String username, String password)
-    {
-        USER = username;
-        PASS = password;
-    }*/
     public void establishConnection()throws SQLException,ClassNotFoundException
     {
     	Class.forName(JDBC_DRIVER);
