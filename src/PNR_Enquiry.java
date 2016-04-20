7bb4d5784eec9e65588b8d97238b69ceac7d4160
vidhan13j07 <vidhanj1307@gmail.com>
2016-04-21 02:59:35 +0530

    updated PNR_Enquiry

--- src/PNR_Enquiry.java
+++ src/PNR_Enquiry.java
@@ -3,52 +3,48 @@ import java.awt.event.*;
 import javax.swing.*;
 class PNR_Enquiry implements  ActionListener {
     JFrame f;
-    JLabel l1,l2,l3;
+    JLabel l1,l2,l3,imgL;
     Main m;
     ImageIcon img;
     JTextField t;
     JButton b1,b2;
     PNR_Enquiry() {
-    img=new ImageIcon("images/indian-rail.jpg");
+    img=new ImageIcon("images/512496759.jpg");
+    imgL = new JLabel(img);
+    imgL.setBounds(0,0,1300,700);
     f=new JFrame("PNR ENQUIRY");
     b1=new JButton("GET DETAILS");
     b1.setForeground(Color.blue);
     b1.addActionListener(this);
-    b1.setBounds(500,500,150,45);
+    b1.setBounds(50,300,150,45);
     
     b2=new JButton("Main Menu");
     b2.setForeground(Color.blue);
     b2.addActionListener(this);
-    b2.setBounds(500,600,150,45);
+    b2.setBounds(200,300,150,45);
     f.getContentPane().add(b1);
     b1.setEnabled(false);
     f.getContentPane().add(b2);
     f.getContentPane().setLayout(null);
-    f.getContentPane().setBackground(Color.yellow);
+    f.getContentPane().setBackground(Color.black);
     l1=new JLabel("PNR Number");
-    l1.setBounds(150,300,500,50);
+    l1.setBounds(50,100,500,50);
     l1.setFont(new Font("Courier new",Font.BOLD,40));
-    l1.setForeground(Color.red);
+    l1.setForeground(Color.white);
     t=new JTextField(10);
     t.setForeground(Color.blue);
-    t.setBounds(450,310,300,40);
+    t.setBounds(50,200,300,40);
     t.addActionListener(this);
     t.addKeyListener(new KeyAdapter() {
-        int previ=-1;
         public void keyTyped(KeyEvent e) {
-            System.out.println(previ + " " + t.getText().length());
-        if (previ+1==t.getText().length()) {
-            System.out.println("AAA "+t.getText().length());
-            // limit textfield to 10 characters
-            if(t.getText().length()==9) b1.setEnabled(true);
-        }
-        if (previ==t.getText().length() ) {
-            b1.setEnabled(false);
-        }
+      
+            
+        if(t.getText().length()>=9) b1.setEnabled(true);
         if(t.getText().length() >=10) e.consume();
-        previ=t.getText().length();
+        if(t.getText().length()<9) b1.setEnabled(false);
         }});
     f.getContentPane().add(t);
+    f.getContentPane().add(imgL);
     f.getContentPane().add(l1);
     f.setSize(1300,1000);
     f.setVisible(true);
